7f07df30f5b33a4d30cca4f8d006bcd9b873da8e
Andres Camilo Torres C <andresctc@gmail.com>
2023-10-05 21:38:14 -0500

    Proyecto base Swagger Spring Boot 3

---
+++ swagger-with-springboot3/src/main/java/com/sacavix/boot3/SwaggerWithSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.sacavix.boot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerWithSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerWithSpringboot3Application.class, args);
+	}
+
+}
---
+++ swagger-with-springboot3/src/main/java/com/sacavix/boot3/controllers/PruebaController.java
@@ -0,0 +1,18 @@
+package com.sacavix.boot3.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class PruebaController {
+
+	@GetMapping
+	public String getAll() {return "all";}
+	
+	@PostMapping
+	public String create(@RequestBody String test) {return test; }
+}
